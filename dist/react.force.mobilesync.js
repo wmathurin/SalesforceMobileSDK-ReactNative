4ef4b159467bfd3d51ee65df39c7215373d6b205
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-05-22 15:36:57 -0700

    Rebuild dist/ with unified callback pattern
    The dist/ compiled JS was stale - tsconfig has noEmit:true so the
    build script only type-checks. Regenerated dist/ to include the
    unified single-callback exec() for both platforms.

--- dist/react.force.mobilesync.js
+++ dist/react.force.mobilesync.js
@@ -1,10 +1,43 @@
 "use strict";
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MERGE_MODE = exports.deleteSync = exports.getSyncStatus = exports.syncUp = exports.cleanResyncGhosts = exports.reSync = exports.syncDown = void 0;
+/*
+ * Copyright (c) 2015-present, salesforce.com, inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ * following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 const react_native_1 = require("react-native");
 const react_force_common_1 = require("./react.force.common");
-const { MobileSyncReactBridge, SFMobileSyncReactBridge } = react_native_1.NativeModules;
+// New architecture: TurboModuleRegistry first, fall back to NativeModules.
+const SFMobileSyncReactBridge = (_a = react_native_1.TurboModuleRegistry.get("SFMobileSyncReactBridge")) !== null && _a !== void 0 ? _a : react_native_1.NativeModules.SFMobileSyncReactBridge;
+const MobileSyncReactBridge = (_b = react_native_1.TurboModuleRegistry.get("MobileSyncReactBridge")) !== null && _b !== void 0 ? _b : react_native_1.NativeModules.MobileSyncReactBridge;
+// If param is a storeconfig return the same storeconfig
+// If param is a boolean, returns a storeconfig object  {'isGlobalStore': boolean}
+// Otherwise, returns a default storeconfig object
 const checkFirstArg = (arg) => {
+    // Turning arguments into array
+    // If first argument is a store config
     if (typeof arg === "object" && Object.prototype.hasOwnProperty.call(arg, "isGlobalStore")) {
         return arg;
     }
@@ -22,6 +55,7 @@ const syncDown = (storeConfig, target, soupName, options, x, y, z) => {
     let syncName;
     let successCB;
     let errorCB;
+    // syncName optional (new in 6.0)
     if (typeof x === "function") {
         syncName = undefined;
         successCB = x;
@@ -66,6 +100,7 @@ const syncUp = (storeConfig, target, soupName, options, x, y, z) => {
     let syncName;
     let successCB;
     let errorCB;
+    // syncName optional (new in 6.0)
     if (typeof x === "function") {
         syncName = undefined;
         successCB = x;
@@ -110,4 +145,3 @@ exports.MERGE_MODE = {
     OVERWRITE: "OVERWRITE",
     LEAVE_IF_CHANGED: "LEAVE_IF_CHANGED",
 };
-//# sourceMappingURL=react.force.mobilesync.js.map
