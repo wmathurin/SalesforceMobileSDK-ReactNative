970422a658193cf0a706c5bc8b20b64e0c0ad37b
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-05-22 17:32:15 -0700

    Add android/ stub and react-native.config.js for autolinking
    The autolinking system needs an android/ directory to detect native
    code. The actual implementation is in SalesforceMobileSDK-Android
    via composite build. This stub enables package discovery.

--- react-native.config.js
+++ react-native.config.js
@@ -4,6 +4,7 @@ module.exports = {
       android: {
         packageImportPath: 'import com.salesforce.androidsdk.reactnative.app.SalesforceReactPackage;',
         packageInstance: 'new SalesforceReactPackage()',
+        sourceDir: './android',
       },
       ios: null,
     },
