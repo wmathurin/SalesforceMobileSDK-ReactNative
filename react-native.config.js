60a5f0855828129abc4f00c0a265a04b51af2da7
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-05-22 21:11:53 -0700

    Set cmakeListsPath to null - no C++ JNI bindings needed

--- react-native.config.js
+++ react-native.config.js
@@ -5,6 +5,7 @@ module.exports = {
         sourceDir: './android',
         packageImportPath: 'import com.salesforce.androidsdk.reactnative.app.SalesforceReactPackage;',
         packageInstance: 'new SalesforceReactPackage()',
+        cmakeListsPath: null,
       },
     },
   },
