f8f3ce859a6e984bdbfa6f2f4ac38b30369b180a
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-06-01 16:25:46 -0700

    Fix CI: ensure ios/ directory exists before writing test_credentials

--- iosTests/create_test_credentials_from_env.js
+++ iosTests/create_test_credentials_from_env.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 if (process.env.TEST_CREDENTIALS) {
     console.log('Writing test credentials from TEST_CREDENTIALS env var');
     fs.writeFileSync('test_credentials.json', process.env.TEST_CREDENTIALS, 'utf8');
+    fs.mkdirSync('ios', {recursive: true});
     fs.writeFileSync('ios/test_credentials.json', process.env.TEST_CREDENTIALS, 'utf8');
 } else {
     console.log('TEST_CREDENTIALS env var not set - skipping');
